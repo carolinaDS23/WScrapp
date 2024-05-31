52c76e30244d2a5422dd616efd1d85507f9f1557
carolinaDS <carolina.garaycochea@itscordoba.edu.ar>
2024-05-31 16:50:52 -0300

    Merge remote changes

---
+++ excep/excepciones.py
@@ -0,0 +1,28 @@
+#cuando caemos en una excepcion 
+# como capturar una excepcion 
+#como devolver un mensaje de error cuando caemos en una excepcion 
+#como controlar varios tipos de excepciones 
+#defender nuestro codigo de las excepciones 
+#el bloque finally
+
+try: 
+    pass
+
+except: 
+
+    pass 
+
+finally: 
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
